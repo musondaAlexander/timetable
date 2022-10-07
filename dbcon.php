80e31b4abb99644b905396c3340231ebee6c5155
musondaalexander <musondaalexander97@gmail.com>
2022-10-07 20:59:52 +0200

    Added the Heroku db to the dbconn file

--- dbcon.php
+++ dbcon.php
@@ -2,10 +2,17 @@
 //--------------------------------------------------------------------------
 
     //  For setting up the connection 
-        $servername = "localhost";
-		$username = "alex"  ;
-		$password = "alEx@2022@zuct";
-		$dbname = "timetabledata";
+        // $servername = "localhost";
+		// $username = "alex"  ;
+		// $password = "alEx@2022@zuct";
+		// $dbname = "timetabledata";
+
+		// THE Heroko connection 
+		$servername = "us-cdbr-east-06.cleardb.net";
+	    $username = "beb5e1d2cd0e49";
+	    $password = "a60fad52";
+	    $dbname = "heroku_3ca43a23c94a4d6";
+
 
 
 		$conn = new mysqli($servername,$username,$password,$dbname);
